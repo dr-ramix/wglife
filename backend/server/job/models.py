409b64239198b4dda749dde237401e0794a7434f
Seyed Ramtin Hosseini <ramtin.hosseini1382@gmail.com>
2025-08-24 21:58:01 +0200

    Adding the current datetime and default end datetime to Work model

--- backend/server/job/models.py
+++ backend/server/job/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from society.models import Clan
 
+from dateime import datetime, timedelta
 
 
 WORK_PERIOD_CHOICES = {
@@ -15,7 +16,8 @@ WORK_PERIOD_CHOICES = {
     "1y"  : "Yearly",
 }
 
-current_datetime = datetime.datetime.now()
+current_datetime = datetime.now()
+default_end_datetime = current_datetime + timedelta(years=5) #current day + 5 years
 
 class Work(models.Model):
     title = models.CharField(max_length=255,
@@ -23,7 +25,7 @@ class Work(models.Model):
                              db_comment="Title of each Work-Template.",
                              help_text="Please use less than 255 characters")
     start = models.DateTimeField(default=current_datetime)
-    end = models.DateTimeField()
+    end = models.DateTimeField(default=default_end_datetime)
     period = models.CharField(choices=WORK_PERIOD_CHOICES, max_length=2, default="1d")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
