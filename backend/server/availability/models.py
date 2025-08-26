9b4006c5d8689ce7d407ec02d75e04da557ebb31
Seyed Ramtin Hosseini <ramtin.hosseini1382@gmail.com>
2025-08-26 17:20:33 +0200

    creating dictionary for dayoff choices

--- backend/server/availability/models.py
+++ backend/server/availability/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
+MODE_OFF_DAY_OFF = {
+     "si":"Sick",
+     "nh":"Not at home",
+     "do":"Day off",
+}
 
 class DayOff(models.Model):
      user = models.ForeignKey(User, on_delete=models.CASCADE ,null=False)
-     mode = models.CharField(max_length=255, null=False, blank=False )
+     mode = models.CharField(max_length=2, null=False, blank=False, choices=MODE_OFF_DAY_OFF)
      date = models.DateField(null=False)
 
