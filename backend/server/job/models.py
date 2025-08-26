77dcb8081cd00654743e3009b8d8f566fc4cefb6
Seyed Ramtin Hosseini <ramtin.hosseini1382@gmail.com>
2025-08-26 17:05:43 +0200

    Romoving wrong attribute min and max. Correcting timedelta

--- backend/server/job/models.py
+++ backend/server/job/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 from django.contrib.auth.models import User
+from rest_framework import status
 from society.models import Clan
 
-from dateime import datetime, timedelta
-
+from datetime import datetime, timedelta
 
 WORK_PERIOD_CHOICES = {
     "1d"  : "Daily",
@@ -15,9 +16,9 @@ WORK_PERIOD_CHOICES = {
     "2m"  : "Each 2 Month",
     "1y"  : "Yearly",
 }
-
+NUMBER_OF_DAYS_IN_5_YEARS = 365 * 5
 current_datetime = datetime.now()
-default_end_datetime = current_datetime + timedelta(years=5) #current day + 5 years
+default_end_datetime = current_datetime + timedelta(days=NUMBER_OF_DAYS_IN_5_YEARS) #current day + 5 years
 
 class Work(models.Model):
     title = models.CharField(max_length=255,
@@ -32,6 +33,7 @@ class Work(models.Model):
     is_infinite = models.BooleanField(default=False)
     created_by = models.ForeignKey(User, on_delete=models.SET_NULL)
     clan = models.ForeignKey(Clan, on_delete=models.CASCADE)
+    duration = models.IntegerField(null=False)
 
     def __self__(self):
         return self.title + self.clan
@@ -50,10 +52,31 @@ class Task(models.Model):
                              db_comment="Title of each Task",
                              help_text="Please use less than 255 characters")
      work = models.ForeignKey(Work, on_delete=models.CASCADE, null=False, blank=False)
-     priority = models.SmallIntegerField(choices=TASK_PRIORITY, max_length=1, min=1, max=4)
+     priority = models.SmallIntegerField(choices=TASK_PRIORITY, validators=[MinValueValidator(1), MaxValueValidator(4)])
 
 
      def __self__(self):
          return self.title + self.work.title + self.work.clean
-     
+
+WORK_ASSIGNMENT_STATUS = {
+     "a" : "Assigned",
+     "u" : "Unassigned",
+     "f" : "Finished",
+     "n" : "Not Done",
+}
+class Assignment(models.Model):
+      assigned_user   = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+      task   = models.ForeignKey(Task, on_delete=models.CASCADE, null=False, blank=False)
+      status = models.CharField(choices=WORK_ASSIGNMENT_STATUS, max_length=1, null=False, blank=False)
+      start   = models.DateTimeField(null=False)
+      end = models.DateTimeField(null=False)
+      finished_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+      def __self__(self):
+           return self.task.title + self.start
+
+class DayOff(models.Model):
+     user = models.ForeignKey(User, on_delete=models.CASCADE ,null=False)
+     mode = models.CharField(max_length=255, null=False, blank=False )
+     date = models.DateField(null=False)
 
