66f0baeb5f3e4e5b46747c43b02c30fe2cc0c91b
Seyed Ramtin Hosseini <ramtin.hosseini1382@gmail.com>
2025-09-12 08:27:28 +0200

    Creating the initial Layout for landing page with navbar and main and footer

--- frontend/web/client/src/pages/landing/landingLayoutPage/LandingLayoutPage.jsx
+++ frontend/web/client/src/pages/landing/landingLayoutPage/LandingLayoutPage.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import NavbarLanding from '../../../components/landing/navbarLanding/NavbarLanding';
+import FooterLanding from '../../../components/landing/footerLanding/FooterLanding';
 const LandingLayoutPage = () => {
     return (
         <div className='landingLayoutPage'>
+            <NavbarLanding />
             <main>
                 <Outlet />
             </main>
+            <FooterLanding />
         </div>
     );
 }
